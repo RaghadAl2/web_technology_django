603a8f32d81e830cde0759dfda964169100e0f86
RaghadAl02 <raghad02ali@gmail.com>
2025-03-09 07:42:22 +0300

    lab5

--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -14,5 +14,18 @@ def viewbook(request):
  
 def aboutus(request):
     return render(request, 'bookmodule/aboutus.html')
- 
 
+def links(request):
+    return render(request, 'bookmodule/html5/links.html')
+
+def formatting(request):
+    return render(request, 'bookmodule/html5/formatting.html')
+ 
+    
+def listing(request):
+    return render(request, 'bookmodule/html5/listing.html')
+ 
+    
+def table(request):
+    return render(request, 'bookmodule/html5/table.html')
+ 
