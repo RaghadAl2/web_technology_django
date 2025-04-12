f47b7300fedf41c57b9ff3c10f14ba1d343383e8
RaghadAl02 <raghad02ali@gmail.com>
2025-04-13 00:37:17 +0300

    lab8

--- apps/bookmodule/models.py
+++ apps/bookmodule/models.py
@@ -5,3 +5,18 @@ class Book(models.Model):
     author = models.CharField(max_length = 50)
     price = models.FloatField(default = 0.0)
     edition = models.SmallIntegerField(default = 1)
+    
+    
+class Address(models.Model):
+    city =models.CharField(max_length = 50)
+ 
+class Student(models.Model):
+    name= models.CharField(max_length = 50)
+    age = models.SmallIntegerField(default = 7)
+    address = models.ForeignKey(Address, verbose_name="Address", on_delete=models.CASCADE)
+
+
+    
+
+    
+
--- apps/bookmodule/views.py
+++ apps/bookmodule/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Book
-
+from .models import Address
+from .models import Student
+from django.db.models import Q, Count, Min, Max, Sum, Avg
 def index(request):
     mybook = Book(title = 'Continuous Delivery', author = 'J.Humble and D. Farley', edition = 1)
     mybook.save()
@@ -12,14 +14,63 @@ def simple_query(request):
     mybooks=Book.objects.filter(title__icontains='and') 
     return render(request, 'bookmodule/bookList.html', {'books':mybooks})
 
-
 def complex_query(request):
     mybooks=books=Book.objects.filter(author__isnull = False).filter(title__icontains='and').filter(edition__gte = 2).exclude(price__lte = 100)[:10]
     if len(mybooks)>=1:
         return render(request, 'bookmodule/bookList.html', {'books':mybooks})
     else:
         return render(request, 'bookmodule/index.html')
+   
+def lab8_task1(request):
+    mybooks=Book.objects.filter(Q(price__lte=80))
+    if len(mybooks)>=1:
+        return render(request, 'bookmodule/bookList.html', {'books':mybooks})
+    else:
+        return render(request, 'bookmodule/index.html')  
+       
+def lab8_task2(request):
+    mybooks=Book.objects.filter(Q(edition__gte=3)&(Q(title__icontains="co")|Q(author__icontains="co")))
+    if len(mybooks)>=1:
+        return render(request, 'bookmodule/bookList.html', {'books':mybooks})
+    else:
+        return render(request, 'bookmodule/index.html')  
     
+         
+def lab8_task3(request):
+    mybooks=Book.objects.filter(~Q(edition__gte=2)&(~Q(title__icontains="ll")|~Q(author__icontains="co")))
+    if len(mybooks)>=1:
+        return render(request, 'bookmodule/bookList.html', {'books':mybooks})
+    else:
+        return render(request, 'bookmodule/index.html')  
+     
+         
+def lab8_task4(request):
+    mybooks=Book.objects.all().order_by('title')
+    if len(mybooks)>=1:
+        return render(request, 'bookmodule/bookList.html', {'books':mybooks})
+    else:
+        return render(request, 'bookmodule/index.html')  
+      
+          
+def lab8_task5(request):
+    query = Book.objects.aggregate(
+    count=Count('id'),
+    total=Sum('price', default=0),
+    average=Avg('price',default=0),
+    max=Max('price',default=0) ,
+    min=Min('price',default=0) ,
+    )
+
+    return render(request, 'bookmodule/books_info.html',{'query':query})
+
+
+
+def lab8_task7(request):
+    query = Student.objects.values('address__city').annotate(total=Count('id'))
+    return render(request, 'bookmodule/Student_list.html',{'query':query})
+
+      
+      
 def list_books(request):
     return render(request, 'bookmodule/list_books.html')
  
