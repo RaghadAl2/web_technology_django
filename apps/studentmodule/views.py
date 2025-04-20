95bfd8aaf19edee6a1785bed21d7b81b7e4aff10
RaghadAl02 <raghad02ali@gmail.com>
2025-04-20 03:31:13 +0300

    lab9

--- apps/studentmodule/views.py
+++ apps/studentmodule/views.py
@@ -4,21 +4,21 @@ from django.shortcuts import render
 from .models import Student, Card, Department, Course
 from django.db.models import Q, Count, Min, Max, Sum, Avg , OuterRef, Subquery
 
-# Create your views here.
+
 
 def lab9_task1(request):
-    query =  Department.objects.annotate(student_count=Count('student_module'))
+    query =  Department.objects.annotate(student_count=Count('student'))
 
     return render(request, 'studentmodule/show_task1.html',{'query':query})
 
 
 def lab9_task2(request):
-    query =  Course.objects.annotate(student_count=Count('student_module'))
+    query =  Course.objects.annotate(student_count=Count('student'))
     return render(request, 'studentmodule/show_task2.html',{'query':query})
 
 
 def lab9_task3(request):
-  departments = Department.objects.annotate(oldest_id=Min('id'))
+  departments = Department.objects.annotate(oldest_id=Min('student__id'))
   return render(request, 'studentmodule/show_task3.html',{'departments':departments})
 
 
